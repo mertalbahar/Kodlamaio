e5c9d7059e64f5c8d975380dadcd2bb5d3eaef18
MertalBahar <mertalbahar@windowslive.com>
2024-02-28 16:06:11 +0300

    Add project files.

---
+++ Kodlamaio.MvcWebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kodlamaio.MvcWebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
